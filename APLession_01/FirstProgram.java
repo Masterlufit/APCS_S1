417f84cdb0d3e3268245982e72c21a113278b6fd
Masterlufit <978034676@qq.com>
2016-09-06 08:07:19 -0700

    Add Files

---
+++ APLession_01/FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram
+{
+	public static void main(String[]args)
+	{
+		System.out.println("Hello World!");
+		System.out.print("Hello World Again~  ");
+		System.out.println("Hello World Again on the same line!");
+		System.out.println("Good Bye \"World\"!");
+	}
+}
